6f48a098c5c041fc6ea45854504644dee385a451
moonelysian <dandyblack@naver.com>
2022-03-08 09:33:59 +0900

    add: pagination 추가

--- todo-list/script.js
+++ todo-list/script.js
@@ -4,21 +4,64 @@
   const get = (target) => {
     return document.querySelector(target);
   };
+  const getAll = (target) => {
+    return document.querySelectorAll(target);
+  };
 
-  const API_URL = "http://localhost:3000/todos";
   const $todos = get(".todos");
   const $form = get(".todo_form");
   const $todoInput = get(".todo_input");
   const $pagination = get(".pagination");
+  const API_URL = `http://localhost:3000/todos`;
 
-  const limit = 5;
+  let currentPage = 1;
   const totalCount = 53;
   const pageCount = 5;
-  let currentPage = 1;
+  const limit = 5;
 
   const pagination = () => {
     let totalPage = Math.ceil(totalCount / limit);
     let pageGroup = Math.ceil(currentPage / pageCount);
+    let lastNumber = pageGroup * pageCount;
+    if (lastNumber > totalPage) {
+      lastNumber = totalPage;
+    }
+    let firstNumber = lastNumber - (pageCount - 1);
+
+    const next = lastNumber + 1;
+    const prev = firstNumber - 1;
+
+    let html = "";
+
+    if (prev > 0) {
+      html += "<button class='prev' data-fn='prev'>이전</button> ";
+    }
+
+    for (let i = firstNumber; i <= lastNumber; i++) {
+      html += `<button class="pageNumber" id="page_${i}">${i}</button>`;
+    }
+    if (lastNumber < totalPage) {
+      html += `<button class='next' data-fn='next'>다음</button>`;
+    }
+
+    $pagination.innerHTML = html;
+    const $currentPageNumber = get(`.pageNumber#page_${currentPage}`);
+    $currentPageNumber.style.color = "#9dc0e8";
+
+    const $currentPageNumbers = getAll(`.pagination button`);
+    $currentPageNumbers.forEach((button) => {
+      button.addEventListener("click", () => {
+        if (button.dataset.fn === "prev") {
+          currentPage = prev;
+        } else if (button.dataset.fn === "next") {
+          currentPage = next;
+        } else {
+          currentPage = button.innerText;
+        }
+        pagination();
+        getTodos();
+      });
+    });
   };
 
   const createTodoElement = (item) => {
@@ -76,12 +119,15 @@
 
   const addTodo = (e) => {
     e.preventDefault();
-    const todo = { content: $todoInput.value, completed: false };
+    const content = $todoInput.value;
+    if (!content) return;
+    const todo = {
+      content,
+      completed: false,
+    };
     fetch(API_URL, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(todo),
     })
       .then((res) => res.json())
@@ -102,11 +148,10 @@
 
     fetch(`${API_URL}/${id}`, {
       method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ completed }),
     })
+      .then((response) => response.json())
       .then(getTodos)
       .catch((error) => console.error(error.message));
   };
@@ -114,15 +159,15 @@
   const changeEditMode = (e) => {
     const $item = e.target.closest(".item");
     const $label = $item.querySelector("label");
-    const $editInput = $item.querySelector("input[type='text']");
+    const $editInput = $item.querySelector('input[type="text"]');
     const $contentButtons = $item.querySelector(".content_buttons");
     const $editButtons = $item.querySelector(".edit_buttons");
     const value = $editInput.value;
 
     if (e.target.className === "todo_edit_button") {
       $label.style.display = "none";
-      $contentButtons.style.display = "none";
       $editInput.style.display = "block";
+      $contentButtons.style.display = "none";
       $editButtons.style.display = "block";
       $editInput.focus();
       $editInput.value = "";
@@ -131,8 +176,8 @@
 
     if (e.target.className === "todo_edit_cancel_button") {
       $label.style.display = "block";
-      $contentButtons.style.display = "block";
       $editInput.style.display = "none";
+      $contentButtons.style.display = "block";
       $editButtons.style.display = "none";
       $editInput.value = $label.innerText;
     }
@@ -141,17 +186,16 @@
   const editTodo = (e) => {
     if (e.target.className !== "todo_edit_confirm_button") return;
     const $item = e.target.closest(".item");
-    const $editInput = $item.querySelector("input[type='text']");
     const id = $item.dataset.id;
+    const $editInput = $item.querySelector('input[type="text"]');
     const content = $editInput.value;
 
     fetch(`${API_URL}/${id}`, {
       method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ content }),
     })
+      .then((response) => response.json())
       .then(getTodos)
       .catch((error) => console.error(error.message));
   };
@@ -164,6 +208,7 @@
     fetch(`${API_URL}/${id}`, {
       method: "DELETE",
     })
+      .then((response) => response.json())
       .then(getTodos)
       .catch((error) => console.error(error.message));
   };
@@ -173,6 +218,7 @@
       getTodos();
       pagination();
     });
+
     $form.addEventListener("submit", addTodo);
     $todos.addEventListener("click", toggleTodo);
     $todos.addEventListener("click", changeEditMode);
