2725800eb9e0c4f2f92a896522c3d2a463159bac
moonelysian <dandyblack@naver.com>
2022-03-17 09:29:34 +0900

    add: 모든 데이터 다 가져오게 일단 설정

--- infinite-scroll/script.js
+++ infinite-scroll/script.js
@@ -4,4 +4,41 @@
   const get = (target) => {
     return document.querySelector(target);
   };
+
+  const $posts = get(".posts");
+
+  const getPosts = async () => {
+    const API_URL = "https://jsonplaceholder.typicode.com/posts";
+    const response = await fetch(API_URL);
+    if (!response.ok) {
+      throw new Error("error!!");
+    }
+    return await response.json();
+  };
+
+  const showPosts = (posts) => {
+    posts.forEach((post) => {
+      const $post = document.createElement("div");
+      $post.classList.add("post");
+      $post.innerHTML = `
+      <div class="header">
+        <div class="id">${post.id}</div>
+        <div class="title">${post.title}</div>
+      </div>
+      <div class="body">
+        ${post.body}
+      </div>
+      `;
+      $posts.appendChild($post);
+    });
+  };
+
+  const loadPosts = async () => {
+    const posts = await getPosts();
+    showPosts(posts);
+  };
+
+  window.addEventListener("DOMContentLoaded", () => {
+    loadPosts();
+  });
 })();
