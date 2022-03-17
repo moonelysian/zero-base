2f149a2b943c155f176c512fcf56f80351fb5e13
moonelysian <dandyblack@naver.com>
2022-03-17 10:02:28 +0900

    add: 무한 스크롤 구현

--- infinite-scroll/script.js
+++ infinite-scroll/script.js
@@ -5,10 +5,16 @@
     return document.querySelector(target);
   };
 
+  let page = 1;
+  const LIMIT = 10;
   const $posts = get(".posts");
+  const end = 100;
+  let total = 10;
+
+  const $loader = get(".loader");
 
   const getPosts = async () => {
-    const API_URL = "https://jsonplaceholder.typicode.com/posts";
+    const API_URL = `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${LIMIT}`;
     const response = await fetch(API_URL);
     if (!response.ok) {
       throw new Error("error!!");
@@ -33,12 +39,42 @@
     });
   };
 
+  const showLoader = () => {
+    $loader.classList.add("show");
+  };
+
+  const hideLoader = () => {
+    $loader.classList.remove("show");
+  };
+
   const loadPosts = async () => {
-    const posts = await getPosts();
-    showPosts(posts);
+    showLoader();
+    try {
+      const posts = await getPosts();
+      showPosts(posts);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      hideLoader();
+    }
+  };
+
+  const onScroll = () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    if (total === end) {
+      window.removeEventListener("scroll", onScroll);
+      return;
+    }
+
+    if (scrollTop + clientHeight >= scrollHeight - 5) {
+      page += 1;
+      total += LIMIT;
+      loadPosts();
+    }
   };
 
   window.addEventListener("DOMContentLoaded", () => {
     loadPosts();
+    window.addEventListener("scroll", onScroll);
   });
 })();
